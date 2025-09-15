c479592bcc9b1751599933046f1bef289e2f98d4
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-09-15 10:59:46 +0530

    completed the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 // import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
-export default function Home() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+export default async function Home() {
+  const{isAuthenticated} = getKindeServerSession()
+  const isLoggedIn = await isAuthenticated()
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center align-center">
-      <h1 className="text-5xl font-bold text-center">
-        Welcome To DailyTM
-      </h1>
-      <div className="flex gap-4">
-        <Button className="mt-4 text-white bg-black font-bold py-2 px-4 rounded-xl">
+     <div className="max-w-7xl mx-auto">
+        <div className="text-center">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight">
+                <p>Your Personal WorkSpace</p>
+                <p className="text-5xl md:text-6xl">
+                    for <span className="text-blue-500">better productivity</span>
+                </p>
+            </h1>
+
+            <p className="mt-6 text-base lg:text-lg text-muted-foreground max-w-2xl mx-auto">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolorum odit facere nihil, saepe omnis impedit sed autem sunt accusantium quas neque, beatae, quod totam. Provident consequuntur ratione rem, sequi eos debitis architecto et saepe sit pariatur excepturi veniam soluta!
+            </p>
+            
+ <div className="flex gap-4 items-center justify-center mt-6">
+        {
+          isLoggedIn ? <>
+
+            <Button asChild >
+                <Link href="/workspace">
+                Goto WorkSpace
+                </Link>
+            </Button>
+          </> : <>
+            <Button >
           <RegisterLink>
             Getting Started
           </RegisterLink>
@@ -19,7 +42,12 @@ export default function Home() {
             SignIn
           </LoginLink>
         </Button>
+          </>
+        }
       </div>
+        </div>
+     </div>
+     
   </div>
 
   );
