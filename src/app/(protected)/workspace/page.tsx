1ce4bcb6a1c27cd255356b46d621246dab123e2c
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-09-16 19:03:31 +0530

    some changes in the schema

--- src/app/(protected)/workspace/page.tsx
+++ src/app/(protected)/workspace/page.tsx
@@ -1,6 +1,21 @@
+import { getUserWorkspaces } from '@/app/data/workspace/get-user-workspaces'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const WorkSpace = () => {
+const WorkSpace = async() => {
+    const {data} = await getUserWorkspaces()
+    // if(!data) re1turn null;
+
+    if(data?.onboardingCompleted && data?.workspaces?.length == 0){
+        redirect("/create-workspace")
+
+    }
+    else if(!data?.onboardingCompleted){
+        redirect("/onboarding")
+    }
+    else{
+        redirect(`/workspace/${data?.workspaces[0].workspaceId}`)
+    }
   return (
     <div>WorkSpace</div>
   )
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,16 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const db = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+// src/lib/db.ts
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // you can also add "query" for debugging
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
