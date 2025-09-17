ba6e3742c2889992dbd2f4b0c214d117c354e76a
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-09-17 10:14:50 +0530

    working on the onboarding page

--- src/app/(protected)/onboarding/page.tsx
+++ src/app/(protected)/onboarding/page.tsx
@@ -1,7 +1,25 @@
-export default function onBoarding(){
+import { userRequired } from "@/app/data/user/is-user-authenticated"
+import { getUserWorkspaces } from "@/app/data/workspace/get-user-workspaces"
+import OnBoardingForm from "@/components/onboardingform"
+import { redirect } from "next/navigation"
+// import onBoardingForm from "@/components/onboardingform"
+export default async function onBoarding(){
+    const {data} = await getUserWorkspaces()
+    const {user} = await userRequired()
+    if(data?.onboardingCompleted && data?.workspaces.length > 0){
+        redirect("/workspace")
+    }
+    else if(data?.onboardingCompleted){
+        redirect("/create-workspace")
+    }
+    const name = `${user?.given_name || "" } ${user?.family_name || ""}`
     return(
         <div>
-            onBoarding page
+            <OnBoardingForm
+                name={name}
+                email = {user?.email as string}
+                image = {user?.picture as string}
+            ></OnBoardingForm>
         </div>
     )
 }
---
+++ src/components/OnBoardingForm.tsx
@@ -0,0 +1,15 @@
+
+interface props  {
+    name:string,
+    email:string
+    image:string
+}
+
+
+export default function OnBoardingForm({name, email}:props){
+    return (
+        <div>
+            onBoardingForm
+        </div>
+    )
+}
